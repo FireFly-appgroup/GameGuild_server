036acb94b1465f98aa6065c27089a87a3c180b9a
Petr Lozhchenko <firefly.app.group@gmail.com>
2018-01-21 15:51:40 +0200

    Start

---
+++ GameGuild/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GameGuild.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
